5e58e22fd7aaf04d70d2b818d8b48f41a34e5fb4
niguskidane <nigus.kidane@gmail.com>
2018-11-06 17:51:10 -0600

    First cut -minimal springboot webapp

---
+++ eLibrary/src/main/java/edu/mum/cs/cs425/demowebapps/eLibrary/eLibrary/ELibraryApplication.java
@@ -0,0 +1,12 @@
+package edu.mum.cs.cs425.demowebapps.eLibrary.eLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibraryApplication.class, args);
+	}
+}
---
+++ eLibrary/src/main/java/edu/mum/cs/cs425/demowebapps/eLibrary/eLibrary/edu/mum/cs/cs425/demowebapps/eLibrary/controller/HomePageController.java
@@ -0,0 +1,15 @@
+package edu.mum.cs.cs425.demowebapps.eLibrary.eLibrary.edu.mum.cs.cs425.demowebapps.eLibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping(value ={ "/","/eLibrary", "/eLibrary/home"})
+    public String displayHomePage(){
+        return "home/index";
+    }
+
+
+}
